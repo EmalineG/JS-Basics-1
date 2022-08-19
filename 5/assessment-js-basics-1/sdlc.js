36a178f9f46f488b73c162c1d821a1b5b5f73aeb
EmalineG <mgayhart0@ivc.edu>
2022-08-19 09:12:02 -0700

    sdlc part of assessment added

---
+++ 5/assessment-js-basics-1/sdlc.js
@@ -0,0 +1,27 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Planning
+// Analysis of Requirements
+// Design
+// Testing & Integration
+// Implementation
+// Maintain
+
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const analysisOfRequirements = "This is the second step, in which all the details required for a software are gathered, and first ideas for a prototype are formed. Developers will typically define these requirements, assess other existing alternatives, and determine needs of users via research and analysis."
+const design = "This is the third step, in which developers outline the structure of the entire application, such as what the user interfaces, system interfaces, network, and databases may need to look like."
+const testingAndIntegration = "This is the fourth step, in which developers actually write the code for the software. Once they have finished coding each section, they must test the code and fix any bugs and potential user issues."
+const implementation = "This is the fifth step, in which the different modules developed are pieced together and implemented into the primary source code. After ensuring there are no further issues, the software should be ready to be made available to end-users."
+const maintain = "This is the sixth and ongoing step, in which developers address any user issues and implement any changes needed. "
