086aea656c1c960229a5c1d0afb1e4886f3ff2a3
EmalineG <mgayhart0@ivc.edu>
2022-08-19 10:03:51 -0700

    assessment with arrays tasks

--- 5/assessment-js-basics-1/arrays.js
+++ 5/assessment-js-basics-1/arrays.js
@@ -43,8 +43,14 @@ const bigOrSmallArray = [1, 99, 42, 69, 102, -10, 159, 352]
 
 // CODE HERE
 
-let answers 
+let answers = []
 
 for(i = 0; i < bigOrSmallArray.length; i++){
-  if(i >)
+  if(bigOrSmallArray[i] > 100){
+    answers.push('big')
+  } else{
+    answers.push('small')
+  }
 }
+
+console.log(answers)
