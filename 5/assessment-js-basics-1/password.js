e3f87518d6b1b26ada5b9fafe7305ad4276304be
EmalineG <mgayhart0@ivc.edu>
2022-08-19 12:06:53 -0700

    assessment with final update

--- 5/assessment-js-basics-1/password.js
+++ 5/assessment-js-basics-1/password.js
@@ -1,8 +1,9 @@
-let password = 'pas32'
+let password = 'passrd123455234532'
 let digitFind = /[0-9]/g
 let letterFind = /[a-z]/g
+let space = " "
 
-if(password.length >= 10 && password.match(digitFind) && password.match(letterFind)){
+if(password.length >= 10 && password.match(digitFind) && password.match(letterFind) && !password.match(space) && !password.includes('password')){
     console.log('yay good job you have a password')
 }else{
     console.log('nope try again')
