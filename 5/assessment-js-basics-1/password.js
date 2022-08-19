81c5bc8d19bf4e8d8550a721ee8fe9e04a0e8cf6
EmalineG <mgayhart0@ivc.edu>
2022-08-19 11:57:12 -0700

    assessment with updated password task

--- 5/assessment-js-basics-1/password.js
+++ 5/assessment-js-basics-1/password.js
@@ -1,7 +1,8 @@
-let password = 'password12'
+let password = 'pas32'
 let digitFind = /[0-9]/g
+let letterFind = /[a-z]/g
 
-if(password.length >= 10 && password.match(digitFind)){
+if(password.length >= 10 && password.match(digitFind) && password.match(letterFind)){
     console.log('yay good job you have a password')
 }else{
     console.log('nope try again')
